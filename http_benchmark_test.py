e681ad03e36b2a76cdc183b0a5094ee0a505d2a9
Ranadeep Singh <ranadeepsingh96@gmail.com>
2024-05-27 21:40:31 -0400

    Add results

--- http_benchmark_test.py
+++ http_benchmark_test.py
@@ -14,7 +14,7 @@ import time
 
 if __name__ == "__main__":
     
-    urls = ['fireworks.ai', 'google.com', 'cupidgpt.ai']
+    urls = ['fireworks.ai', 'vercel.com', 'cupidgpt.ai']
     qpss = [1, 10, 100, 1000]
     timeouts = [10, 60]
     durations = [5, 30, 60]
@@ -25,20 +25,22 @@ if __name__ == "__main__":
     # durations = [5]
 
     # List of all possible combinations of parameters
-    params = [(url, qps, timeout, duration) for url in urls for qps in qpss for timeout in timeouts for duration in durations]
+    params = [(url, qps, timeout, duration) for qps in qpss for timeout in timeouts for duration in durations for url in urls]
 
     results = []
-    
-    for url, qps, timeout, duration in params:
-        print(f"Testing URL: {url}, QPS: {qps}, Timeout: {timeout}, Duration: {duration}")
-        benchmark = HTTPBenchmark(url, qps, timeout)
-        benchmark.start_test(duration)
-        results.append(benchmark.get_results())
-        
-    # Save results to a CSV file
+
     with open('results.csv', 'w') as f:
         f.write(','.join(['URL', 'QPS', 'Timeout', 'Duration', 'Actual QPS', 'Num Requests', 'Num Successful', 'Num Failed', 'Error Rate %', 'Average Latency (ms)', 'Max Latency (ms)', 'Min Latency (ms)', 'Error Count', 'Top Errors']) + '\n')
-        for result in results:
-            f.write(f"{','.join(map(str,result))}\n")
+    
+        for url, qps, timeout, duration in params:
+            print(f"Testing URL: {url}, QPS: {qps}, Timeout: {timeout}, Duration: {duration}")
+            benchmark = HTTPBenchmark(url, qps, timeout)
+            benchmark.start_test(duration)
+            results.append(benchmark.get_results())
+            f.write(f"{','.join(map(str,results[-1]))}\n")
+            # Sleep for 10 sec between tests
+            time.sleep(10)
+    
+            
 
     
